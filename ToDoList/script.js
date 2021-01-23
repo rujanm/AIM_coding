628a7afd0742099cfe29d55fea34f6c60d819a03
rujanm <rujan.m@gmail.com>
2021-01-23 13:37:45 -0600

    commit

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
     $("#home").click(function(){
-        $("#ToDoList_Home").append("<p id ='added'>" + $("#input").val() + "</p>")
+        $("#ToDoList_Home").append("<p class ='added'>" + $("#input").val() + "</p>")
     })
     $("#work").click(function(){
-        $("#ToDoList_Work").append("<p id ='added'>" + $("#input").val() + "</p>")
+        $("#ToDoList_Work").append("<p class ='added'>" + $("#input").val() + "</p>")
     })
     $("#clearAll").click(function(){
         $('#ToDoList_Home').find('*').not('h1').remove()
         $('#ToDoList_Work').find('*').not('h1').remove()
     })
     
-    $("p").click(function() {
-        $(this).toggleClass("line");
-      });
+    $('body').on
 })
 
