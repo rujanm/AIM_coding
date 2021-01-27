c8002f7015c9a68f121afa409ec8d6d271fad332
rujanm <rujan.m@gmail.com>
2021-01-26 18:06:57 -0600

    commit

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,15 +1,34 @@
 $(document).ready(function(){
-    $("#home").click(function(){
-        $("#ToDoList_Home").append("<p class ='added'>" + $("#input").val() + "</p>")
-    })
-    $("#work").click(function(){
-        $("#ToDoList_Work").append("<p class ='added'>" + $("#input").val() + "</p>")
-    })
-    $("#clearAll").click(function(){
-        $('#ToDoList_Home').find('*').not('h1').remove()
-        $('#ToDoList_Work').find('*').not('h1').remove()
-    })
+    let i = 1;
+    let j = 1;
     
-    $('body').on
-})
+    $("#home").click(function(){
+        if($("#input").val() == ""){
+            alert("Write something!");
+        }
+        else{
+            $("#ToDoList_Home").append("<p class ='added' id='" + i + "'>"  + $("#input").val() + "</p>");
+            i++;
+        }
+        
+    });
+    $("#work").click(function(){
+        if($("#input").val() == ""){
+            alert("Write something!");
+        }
+        else{
+            $("#ToDoList_Work").append("<p class ='added' id='" + j + "'>"  + $("#input").val() + "</p>");
+            j++;
+        }
+        
+    });
+    $("#clearAll").click(function(){
+        $('#ToDoList_Home').find('*').not('h1').remove();
+        $('#ToDoList_Work').find('*').not('h1').remove();
+    });
+    
+    $('body').on("click", ".added", function(){
+        $(this).remove();
+    });
+});
 
